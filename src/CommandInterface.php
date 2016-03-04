589327616f6fdba76420ff3c4b82d4cdb91da131
Radek Adamiec <radek@code-mine.com>
2016-03-04 10:20:43 +0100

    Added interfaces for distinguishing commands from queries

---
+++ src/CommandInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radek
+ * Date: 04.03.16
+ * Time: 10:18
+ */
+
+namespace CodeMine\CommandQuery;
+
+/**
+ * This interface is only used for distinguish command from query
+ * Class CommandInterface
+ *
+ * @package CodeMine\CommandQuery
+ */
+interface CommandInterface
+{
+
+}
---
+++ src/QueryInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radek
+ * Date: 04.03.16
+ * Time: 10:18
+ */
+
+namespace CodeMine\CommandQuery;
+
+/**
+ * This interface is only used for distinguish query from command
+ * Interface QueryInterface
+ *
+ * @package CodeMine\CommandQuery
+ */
+interface QueryInterface
+{
+
+}
